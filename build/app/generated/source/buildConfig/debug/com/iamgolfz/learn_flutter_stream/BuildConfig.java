83190417a27f75b28a8beb7b4b8354f65514fd77
Surattikorn Chumkaew <surattikorn@gmail.com>
2020-02-23 00:16:34 +0700

    add files

---
+++ build/app/generated/source/buildConfig/debug/com/iamgolfz/learn_flutter_stream/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.iamgolfz.learn_flutter_stream;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.iamgolfz.learn_flutter_stream";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
